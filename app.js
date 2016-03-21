4d5f28e6d92dcbcfa5456ac0adb005669eb446df
nai94 <naiyarah.h@gmail.com>
2016-03-21 17:21:05 +0400

    error handling added to app.js

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 
 // set up ======================================================================
 // get all the tools we need
-var express  = require('express');
-var app      = express();
+var express = require('express');
+var app = express();
 var mongoose = require('mongoose');
 var passport = require('passport');
 var path = require('path');
-var flash    = require('connect-flash');
+var flash = require('connect-flash');
 var bodyParser = require('body-parser');
 
 var configDB = require('./config/database.js');
@@ -17,24 +17,55 @@ mongoose.connect(configDB.url); // connect to our database
 
 require('./config/passport')(passport); // pass passport for configuration
 
-app.configure(function() {
+app.configure(function () {
 
-  // set up our express application
-  app.use(express.logger('dev')); // log every request to the console
-  app.use(express.cookieParser()); // read cookies (needed for auth)
-  app.use(express.bodyParser()); // get information from html forms
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(express.static(path.join(__dirname, 'public')));
+    // set up our express application
+    app.use(express.logger('dev')); // log every request to the console
+    app.use(express.cookieParser()); // read cookies (needed for auth)
+    app.use(express.bodyParser()); // get information from html forms
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(express.static(path.join(__dirname, 'public')));
 
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'ejs');
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
 
-  // required for passport
-  app.use(express.session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
-  app.use(passport.initialize());
-  app.use(passport.session()); // persistent login sessions
-  app.use(flash()); // use connect-flash for flash messages stored in session
+    // required for passport
+    app.use(express.session({secret: 'ilovescotchscotchyscotchscotch'})); // session secret
+    app.use(passport.initialize());
+    app.use(passport.session()); // persistent login sessions
+    app.use(flash()); // use connect-flash for flash messages stored in session
+
+/*// catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });*/
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+    if (app.get('env') === 'development') {
+        app.use(function (err, req, res, next) {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: err
+            });
+        });
+    }
+
+// production error handler
+// no stacktraces leaked to user
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
+    });
 
 });
 
